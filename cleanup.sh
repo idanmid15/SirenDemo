ae88c76b6123ccb5892d4fd9b32b36b7cf0cf69e
Idan Midroni <idanmid15@gmail.com>
2019-01-31 14:04:42 +0200

    Tweaked the istio version

--- cleanup.sh
+++ cleanup.sh
@@ -11,8 +11,8 @@
 #kubectl delete --all deployments
 #kubectl delete --all deployments -n istio-system
 
-kubectl delete -f istio-1.0.3/install/kubernetes/istio-demo.yaml
-kubectl delete -f istio-1.0.3/install/kubernetes/helm/istio/templates/crds.yaml -n istio-system
+kubectl delete -f istio-1.0.5/install/kubernetes/istio-demo.yaml
+kubectl delete -f istio-1.0.5/install/kubernetes/helm/istio/templates/crds.yaml -n istio-system
 echo "******Deleted Istio******"
 sudo minikube delete
 echo "******Stopped Minikube******"
