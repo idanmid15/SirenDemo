bbcf2d5151bfff568aee4a30ee9a7892084d6c00
Idan Midroni <idanmid15@gmail.com>
2019-02-02 17:37:35 +0200

    Added basic message indicating the server started

--- creditcard_db.py
+++ creditcard_db.py
@@ -22,6 +22,7 @@ class S(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=S, port=8075):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
+    print("Started serving on localhost:{}".format(port))
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
