42976d080c2c73167cb39a3acf4868dd69819457
Idan Midroni <idanmid15@gmail.com>
2019-02-04 12:53:42 +0200

    Changed the credit card db to be implemented in Flask. Added a "backdoor" left open by "mistake" Added a credit card db file

---
+++ creditcards/creditcard_db.py
@@ -0,0 +1,31 @@
+
+import sys
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+
+def load_file_to_json(filename):
+    with open(filename) as json_file:
+        return json.load(json_file)
+
+
+@app.route('/details/<product_id>')
+def get_all_purchases(product_id):
+
+    # This is a backdoor used by developers to get all purchase details for testing purposes. Was left open in prod.
+    if int(product_id) == 372:
+        return json.dumps(load_file_to_json('cards.json')), 200, {'Content-Type': 'application/json'}
+    else:
+        return json.dumps({"details": "The greatest book ever read"}), 200, {'Content-Type': 'application/json'}
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("usage: %s port" % (sys.argv[0]))
+        sys.exit(-1)
+
+    port = int(sys.argv[1])
+    print("started at port {}".format(port))
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
